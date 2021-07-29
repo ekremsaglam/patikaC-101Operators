6ca7b293b3d6428e0bd0347a98d1f94eb27fb420
Ekrem Sağlam <ekremsaglammm@gmail.com>
2021-07-29 16:05:42 +0300

    Final Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace operators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x =3;
+            int y= 3;
+            Console.WriteLine(y);
+
+            y += 3;
+            Console.WriteLine(y);
+            
+            y /= 3;
+            Console.WriteLine(y);
+
+            //Mantıksal operatörler
+            // || , && , !
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if(isSuccess && isCompleted){
+                Console.WriteLine("isSuccess");
+            }
+            if (isSuccess|| isCompleted){
+                Console.WriteLine("Great");
+
+            }
+            if(isSuccess && !isCompleted){
+                Console.WriteLine("Fine");
+            }
+
+            int number1 = 10;
+            int number2 = 5;
+            int sonuc1 =number1/number2;
+            int sonuc2 = number1* number2;
+            number1++;
+            
         }
     }
 }
